c9ab54c5b6cdf7a5a7cbab9ca2dff19ac612ab34
Jesus Sanchez <sanchez.moral.jesus@gmail.com>
2021-11-12 17:42:33 +0100

    Seguimos con el Front

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from './components/header/Header'
+import SearchFilter from './components/searchFilter/SearchFilter'
 
 const App = () => {
   return (
     <div className="App">
       <Header />
+      <SearchFilter />
     </div>
   );
 }
---
+++ src/components/searchFilter/SearchFilter.jsx
@@ -0,0 +1,30 @@
+import "./searchFilter.css"
+import Search from '../../assets/imgs/search.png'
+
+const SearchFilter = () => {
+  return (
+    <div className="sf">
+      <div className="sf-wrapper">
+        <div className="sf-left">
+          <img src={Search} alt="" />
+          <input 
+            type="text" 
+            className="sf-left-search" 
+            placeholder="Search for a country..." />
+        </div>
+        <div className="sf-rigth">
+          <select name="" id="countries" className="sf-rigth-filter">
+            <option style={{display: "none"}} disabled selected>Filter by Region</option>
+            <option value="Africa">Africa</option>
+            <option value="America">America</option>
+            <option value="Asia">Asia</option>
+            <option value="Europe">Europe</option>
+            <option value="Oceania">Oceania</option>
+          </select>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default SearchFilter
