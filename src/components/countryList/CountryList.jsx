c4a9425198fbc59275d7926b2ed99ef1da19d2af
Jesus Sanchez <sanchez.moral.jesus@gmail.com>
2021-11-17 17:05:54 +0100

    Implemented search function

--- src/components/countryList/CountryList.jsx
+++ src/components/countryList/CountryList.jsx
@@ -1,21 +1,60 @@
-import './countryList.css'
-import CountryCard from '../countryCard/CountryCard'
+import "./countryList.css";
+import CountryCard from "../countryCard/CountryCard";
+import { useState, useEffect } from "react";
+import getApi from "../../services/getApi";
 
-const CountryList = ({countries}) => {
-    console.log(countries)
+const CountryList = ({ search }) => {
+  const [countries, setCountries] = useState([]);
+  const [countriesSearched, setCountriesSearched] = useState();
 
-    return (
+  useEffect(() => {
+    if (search === "") {
+      getApi.getAllCountries(setCountries);
+    }
+  }, [search]);
+
+  useEffect(() => {
+    const filterCountry = countries.filter(
+      (x) => x.name.common.toLowerCase().indexOf(search.toLowerCase()) > -1
+    );
+    setCountriesSearched(filterCountry);
+  }, [search]);
+
+  return (
+    <>
+      {countriesSearched === undefined || search === "" ? (
         <div className="cl">
-            <div className="cl-wrapper">
-                {
-                    countries.map((x) => (
-                        <CountryCard key={x.name.common} name={x.name.common} flag={x.flags.png} population={x.population} region={x.region} capital={x.capital}/>        
-                    ))
-                }
-                
-            </div>
+          <div className="cl-wrapper">
+            {countries.map((x) => (
+              <CountryCard
+                key={x.name.common}
+                name={x.name.common}
+                flag={x.flags.png}
+                population={x.population}
+                region={x.region}
+                capital={x.capital}
+              />
+            ))}
+          </div>
         </div>
-    )
-}
+      ) : (
+        <div className="cl">
+          <div className="cl-wrapper">
+            {countriesSearched.map((x) => (
+              <CountryCard
+                key={x.name.common}
+                name={x.name.common}
+                flag={x.flags.png}
+                population={x.population}
+                region={x.region}
+                capital={x.capital}
+              />
+            ))}
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
 
-export default CountryList
+export default CountryList;
--- src/components/searchFilter/SearchFilter.jsx
+++ src/components/searchFilter/SearchFilter.jsx
@@ -1,35 +1,16 @@
 import "./searchFilter.css";
 import Search from "../../assets/imgs/search.png";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import CountryList from "../countryList/CountryList";
-import getApi from "../../services/getApi";
 
 const SearchFilter = () => {
 
-  const [search, setSearch] = useState();
-  const [data, setData] = useState([]);
-  const [countries, setCountries] = useState('hola')
-
-  console.log(countries)
-
-  useEffect(() =>{
-      getApi.getAllCountries(setCountries)
-  }, [])
-
-  /* useEffect(() => {
-    handleChange()
-  }, [search]) */
+  const [search, setSearch] = useState("");
+  
 
   const handleChange = async (e) => {
     setSearch(e.target.value)
     console.log(search)
-    if(search && search !== []){
-      const mapCountry = countries.map(x => x.name.common)
-      const filterCountry = mapCountry.filter((x) => (
-        x.toLowerCase().indexOf(search.toLowerCase()) > -1
-    ))
-      getApi.getCountryByName(filterCountry, setData)
-      }
   }
 
   return (
@@ -42,9 +23,7 @@ const SearchFilter = () => {
               type="text"
               className="sf-left-search"
               placeholder="Search for a country..."
-              onChange={(e) => {
-                handleChange(e);
-              }}
+              onChange={(e) => handleChange(e)}
             />
           </div>
           <div className="sf-rigth">
@@ -61,11 +40,7 @@ const SearchFilter = () => {
           </div>
         </div>
       </div>
-      {
-      data && data !== ""
-      ? <CountryList countries={data}/>
-      : <CountryList countries={countries}/>
-      }     
+      <CountryList search={search} />
     </>
   );
 };
