c11297da61a63964d77d735bf9cc1c8a31b4cb3f
Jesus Sanchez <74028565+Sanchez9aa@users.noreply.github.com>
2021-12-14 13:52:52 +0100

    Update BorderCountry.jsx

--- src/components/borderCountry/BorderCountry.jsx
+++ src/components/borderCountry/BorderCountry.jsx
@@ -1,25 +1,38 @@
-import {useEffect, useState} from 'react'
-import { Link } from 'react-router-dom'
-import getApi from '../../services/getApi'
-import './borderCountry.css'
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import getApi from "../../services/getApi";
+import "./borderCountry.css";
 import { useContext } from "react";
-import { DarkContext} from "../../contextApi";
+import { DarkContext } from "../../contextApi";
 
-const BorderCountry = ({countries}) => {
+const BorderCountry = ({ countries }) => {
+  const dark = useContext(DarkContext);
 
-  const dark = useContext(DarkContext)
+  const [data, setData] = useState([]);
 
-  const [data, setData] = useState([])
-
-  useEffect(()=>{
-    getApi.getCountryByCCA3(countries, setData)
-  },[countries])
+  useEffect(() => {
+    getApi.getCountryByCCA3(countries, setData);
+  }, [countries]);
 
   return (
-    
-    <span className={!dark.state.darkmode ? "bc" : "bc shadow darkEL"}><Link className={!dark.state.darkmode ? null : "darkColor"} to={`/${data}`}>{data}</Link></span>
-    
-  )
-}
+    <>
+      {!countries ? (
+        <span className={!dark.state.darkmode ? "" : "darkColor"}>
+          {" "}
+          Do not have border countries
+        </span>
+      ) : (
+        <span className={!dark.state.darkmode ? "bc" : "bc shadow darkEL"}>
+          <Link
+            className={!dark.state.darkmode ? null : "darkColor"}
+            to={`/${data}`}
+          >
+            {data}
+          </Link>
+        </span>
+      )}
+    </>
+  );
+};
 
-export default BorderCountry
+export default BorderCountry;
